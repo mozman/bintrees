c074995f4b591e3f8c19248f291eaca5b7577d5b
mozman <mozman@gmx.at>
2010-05-01 06:25:51 +0200

    working on jsw trees

--- tests/test_avltree.py
+++ tests/test_avltree.py
@@ -1,166 +1,17 @@
 #!/usr/bin/env python
 #coding:utf-8
 # Author:  mozman
-# Purpose: test binary trees
+# Purpose: test AVLTrees
 # Created: 28.04.2010
 
-import sys
-import unittest2 as unittest
-from random import randint
+import unittest
 
+from tree_test import TestAbstTree
 from bintrees.avltree import AVLTree
 
-import dxfwrite
-def draw_tree(tree, fname, key=-1):
-    def draw_node(node, pos, dx):
-        dwg.add(dxf.text(str(node.key), halign=dxfwrite.CENTER, insert=pos, alignpoint=pos, height=0.1))
-        if node.left is not None:
-            pos2 = (pos[0]-dx, pos[1]+dy)
-            dwg.add(dxf.line(pos, pos2))
-            draw_node(node.left, pos2, dx*fact)
-        if node.right is not None:
-            pos3 = (pos[0]+dx, pos[1]+dy)
-            dwg.add(dxf.line(pos, pos3))
-            draw_node(node.right, pos3, dx*fact)
-    dy = 1
-    fact = 0.45
-    dxf = dxfwrite.DXFEngine
-    dwg = dxf.drawing(fname)
-    if key != -1:
-        dwg.add(dxf.text('before remove key: ' + str(key), insert=(100, 95)))
-    draw_node(tree.root, (100, 100), 40)
-    return dwg
-
-class TestAVLTree(unittest.TestCase):
-    default_values1 = zip([12, 34, 45, 16, 35, 57], [12, 34, 45, 16, 35, 57])
-    default_values2 = [(2, 12), (4, 34), (8, 45), (1, 16), (9, 35), (3, 57)]
-
-    def test_create_tree(self):
-        tree = AVLTree()
-        self.assertEqual(len(tree), 0)
-        tree.update(self.default_values1)
-        self.assertEqual(len(tree), 6)
-
-    def test_iter_tree(self):
-        tree = AVLTree(self.default_values1)
-        result = list(iter(tree))
-        self.assertEqual(result, [12, 16, 34, 35, 45, 57])
-
-    def test_get_items(self):
-        tree = AVLTree(self.default_values1)
-        self.assertEqual(tree.get(34), 34)
-        self.assertEqual(tree.get(12), 12)
-
-    def test_remove(self):
-        keys = [50, 25, 20, 35, 22, 23, 27, 75, 65, 90, 60, 70, 85, 57, 83]
-        data = dict.fromkeys(keys)
-        for remove_key in keys:
-            tree = AVLTree.fromkeys(keys)
-            tree.remove(remove_key)
-            for search_key in keys:
-                if search_key == remove_key:
-                    self.assertFalse(search_key in tree)
-                else:
-                    self.assertTrue(search_key in tree)
-
-    def test_delete_node(self):
-        tree = AVLTree(self.default_values1)
-        del tree[57]
-        self.assertFalse(57 in tree)
-
-    def test_random_numbers(self):
-        keys = set([randint(0, 10000) for _ in xrange(1000)])
-        #with open('testkey.txt') as fp:
-        #    keys = eval(fp.read())
-        tree = AVLTree.fromkeys(keys)
-        #draw_tree(tree, 'avltree.dxf').save()
-        self.assertEqual(len(tree), len(keys))
-        keystack = []
-        for key in keys:
-            if key == 4564:
-                with open('keystack.txt', 'w') as fp:
-                    fp.write(repr(keystack))
-                draw_tree(tree, '{0}.dxf'.format(key)).save()
-            del tree[key]
-            keystack.append(key)
-        self.assertEqual(len(tree), 0)
-        self.assertTrue(tree.is_empty())
-
-    def test_order(self):
-        keys = set([randint(0, 10000) for _ in xrange(1000)])
-        tree = AVLTree.fromkeys(keys)
-        generator = iter(tree)
-        a = generator.next()
-        for b in generator:
-            self.assertGreaterEqual(b, a)
-            a = b
-
-    def test_index_operator(self):
-        tree = AVLTree(self.default_values1)
-        self.assertEqual(tree[45], 45)
-        tree[99] = 99
-
-    def test_complex_data(self):
-        tree = AVLTree(self.default_values2)
-        self.assertEqual(tree[8], 45)
-
-    def test_default_vaule(self):
-        tree = AVLTree()
-        self.assertEqual(tree.get(7, "DEFAULT"), "DEFAULT")
-
-    def test_has_key(self):
-        tree = AVLTree(self.default_values2)
-        self.assertTrue(tree.has_key(8))
-        self.assertFalse(tree.has_key(11))
-        self.assertTrue( 8 in tree)
-        self.assertFalse(8 not in tree)
-        self.assertFalse(11 in tree)
-        self.assertTrue(11 not in tree)
-
-    def test_pop(self):
-        tree = AVLTree(self.default_values2)
-        data = tree.pop(8)
-        self.assertEqual(data, 45)
-        self.assertFalse(8 in tree)
-        self.assertRaises(KeyError, tree.pop, 8)
-        self.assertEqual(tree.pop(8, 99), 99)
-
-    def test_popitem(self):
-        tree = AVLTree(self.default_values2)
-        d = dict()
-        while not tree.is_empty():
-            key, value = tree.popitem()
-            d[key]=value
-        expected = {2: 12, 4: 34, 8: 45, 1: 16, 9: 35, 3: 57}
-        self.assertEqual(expected, d)
-        self.assertRaises(KeyError, tree.popitem)
-
-    def test_iterkeys(self):
-        tree = AVLTree(self.default_values2)
-        keys = tree.keys()
-        expected = [1, 2, 3, 4, 8, 9]
-        self.assertEqual(expected, keys)
-
-    def test_itervalues(self):
-        tree = AVLTree(self.default_values2)
-        values = tree.values()
-        expected = [16, 12, 57, 34, 45, 35]
-        self.assertEqual(expected, values)
-
-    def test_copy(self):
-        tree = AVLTree(self.default_values2)
-        copytree = tree.copy()
-        self.assertEqual(tree.items(), copytree.items())
-
-    def test_clear(self):
-        tree = AVLTree(self.default_values2)
-        tree.clear()
-        self.assertEqual(tree.root, None)
-
-    def test_to_dict(self):
-        tree = AVLTree(self.default_values2)
-        d = dict(tree)
-        self.assertEqual(d, dict(self.default_values2))
+class TestAVLTree(TestAbstTree):
+    def setUp(self):
+        self.TREE = AVLTree
 
 if __name__=='__main__':
     unittest.main()
--- tests/test_cbintree.py
+++ tests/test_cbintree.py
@@ -1,162 +1,21 @@
 #!/usr/bin/env python
 #coding:utf-8
 # Author:  mozman
-# Purpose: test binary trees
+# Purpose: test cython binary trees
 # Created: 28.04.2010
 
-import sys
-import unittest2 as unittest
-from random import randint
+import unittest
 
 if sys.platform.startswith('linux2'):
     import pyximport
     pyximport.install()
 
+from tree_test import TestAbstTree
 from bintrees.ctrees import cBinaryTree
 
-class TestBinaryTree(unittest.TestCase):
-    default_values1 = zip([12, 34, 45, 16, 35, 57], [12, 34, 45, 16, 35, 57])
-    default_values2 = [(2, 12), (4, 34), (8, 45), (1, 16), (9, 35), (3, 57)]
-    def test_create_tree(self):
-        tree = cBinaryTree()
-        self.assertEqual(len(tree), 0)
-        tree.update(self.default_values1)
-        self.assertEqual(len(tree), 6)
-
-    def test_iter_tree(self):
-        tree = cBinaryTree(self.default_values1)
-        result = list(iter(tree))
-        self.assertEqual(result, [12, 16, 34, 35, 45, 57])
-
-    def test_get_items(self):
-        tree = cBinaryTree(self.default_values1)
-        self.assertEqual(tree.get(34), 34)
-        self.assertEqual(tree.get(12), 12)
-
-    def test_remove_inner_node(self):
-        tree = cBinaryTree(self.default_values1)
-        tree.remove(34)
-        self.assertTrue(12 in tree)
-        self.assertTrue(16 in tree)
-        self.assertTrue(35 in tree)
-        self.assertTrue(45 in tree)
-        self.assertTrue(57 in tree)
-        self.assertTrue(len(tree), 5)
-
-    def test_remove_root(self):
-        tree = cBinaryTree(self.default_values1)
-        tree.remove(12)
-        self.assertFalse(12 in tree)
-        self.assertTrue(16 in tree)
-        self.assertTrue(34 in tree)
-        self.assertTrue(35 in tree)
-        self.assertTrue(45 in tree)
-        self.assertTrue(57 in tree)
-        self.assertEqual(len(tree), 5)
-
-    def est_remove_leaf_node(self):
-        tree = cBinaryTree(self.default_values1)
-        tree.remove(57)
-        node = tree._find_node(tree.root, 45)
-        self.assertEqual(node.left.value, 35)
-        self.assertEqual(node.right, None)
-        self.assertEqual(len(tree), 5)
-
-    def est_delete_node(self):
-        tree = cBinaryTree(self.default_values1)
-        del tree[57]
-        node = tree._find_node(tree.root, 45)
-        self.assertEqual(node.left.value, 35)
-        self.assertEqual(node.right, None)
-        self.assertEqual(len(tree), 5)
-
-    def est_random_numbers(self):
-        tree = cBinaryTree()
-        for x in xrange(1000):
-            val = randint(0, 10000)
-            tree[val] = val
-        self.assertGreater(len(tree), 0)
-        generator = iter(tree)
-        a = generator.next()
-        for b in generator:
-            self.assertGreaterEqual(b, a)
-        count = tree._count
-        tree.count() # recount
-        self.assertEqual(count, len(tree))
-
-    def est_index_operator(self):
-        tree = cBinaryTree(self.default_values1)
-        self.assertEqual(tree[45], 45)
-        tree[99] = 99
-
-    def est_complex_data(self):
-        tree = cBinaryTree(self.default_values2)
-        self.assertEqual(tree[8], 45)
-
-    def est_default_vaule(self):
-        tree = cBinaryTree(self.default_values2)
-        self.assertEqual(tree.get(7, "DEFAULT"), "DEFAULT")
-
-    def test_has_key(self):
-        tree = cBinaryTree(self.default_values2)
-        self.assertTrue(tree.has_key(8))
-        self.assertFalse(tree.has_key(11))
-        self.assertTrue( 8 in tree)
-        self.assertFalse(8 not in tree)
-        self.assertFalse(11 in tree)
-        self.assertTrue(11 not in tree)
-
-    def est_pop(self):
-        tree = cBinaryTree(self.default_values2)
-        data = tree.pop(8)
-        self.assertEqual(data, 45)
-        self.assertFalse(8 in tree)
-        self.assertRaises(KeyError, tree.pop, 8)
-        self.assertEqual(tree.pop(8, 99), 99)
-
-    def est_popitem(self):
-        tree = cBinaryTree(self.default_values2)
-        d = dict()
-        while not tree.is_empty():
-            key, value = tree.popitem()
-            d[key]=value
-        expected = {2: 12, 4: 34, 8: 45, 1: 16, 9: 35, 3: 57}
-        self.assertEqual(expected, d)
-        self.assertRaises(KeyError, tree.popitem)
-
-    def est_iterkeys(self):
-        tree = cBinaryTree(self.default_values2)
-        keys = tree.keys()
-        expected = [1, 2, 3, 4, 8, 9]
-        self.assertEqual(expected, keys)
-
-    def est_itervalues(self):
-        tree = cBinaryTree(self.default_values2)
-        values = tree.values()
-        expected = [16, 12, 57, 34, 45, 35]
-        self.assertEqual(expected, values)
-
-    def est_copy(self):
-        tree = cBinaryTree(self.default_values2)
-        copytree = tree.copy()
-        self.assertEqual(tree.items(), copytree.items())
-
-    def est_to_dict(self):
-        tree = cBinaryTree(self.default_values2)
-        d = dict(tree)
-        self.assertEqual(d, dict(self.default_values2))
-
-    def est_clear(self):
-        tree = cBinaryTree(self.default_values2)
-        tree.clear()
-        self.assertTrue(tree.is_empty())
-
-    def est_setdefault(self):
-        tree = cBinaryTree(self.default_values2)
-        value = tree.setdefault(2, 17) # key <2> exists and == 12
-        self.assertEqual(value, 12)
-        value = tree.setdefault(99, 77)
-        self.assertEqual(value, 77)
+class TestcBinaryTree(TestAbstTree):
+    def setUp(self):
+        self.TREE = cBinaryTree
 
 if __name__=='__main__':
     unittest.main()
