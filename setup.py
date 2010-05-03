f90fbac227c6488c8cc961dc1d3dc41a8df03af6
Manfred Moitzi <mozman@gmx.at>
2010-05-03 15:30:40 +0200

    version v0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name = 'bintrees',
-    version='0.1.0',
+    version='0.2.0',
     description='Package provides Binary-, RedBlack- and AVL-Trees in Python and Cython.',
     author='mozman',
     url='http://bitbucket.org/mozman/bintrees',
