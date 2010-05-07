e46083130831e5fba9bef99cb132cab7006caf63
Manfred Moitzi <mozman@gmx.at>
2010-05-07 06:39:08 +0200

    c version

--- bintrees/node.c
+++ bintrees/node.c
@@ -1,36 +1,67 @@
 #include "node.h"
 #include <Python.h>
 
-#define LEFT 0
-#define RIGHT 1
-
 node_t * node_new(PyObject *key, PyObject *value)
 {
-    node_t * new_node = PyMem_Malloc(sizeof(*new_node))
-    if new_node != NULL) {
-        new_node->key = key;
-        Py_INCREF(key);
-        new_node->value = value;
-        Py_INCREF(value)
-        new_node->link[LEFT] = NULL;
-        new_node->link[RIGHT] = NULL;
-        new_node->xdata = 0;
-    }
-    return new_node
+    node_t * new_node = PyMem_Malloc(sizeof(*new_node));
+    if (new_node != NULL)
+        {
+            KEY(new_node) = key;
+            Py_INCREF(key);
+            VALUE(new_node) = value;
+            Py_INCREF(value);
+            LEFT_NODE(new_node) = NULL;
+            RIGHT_NODE(new_node) = NULL;
+            new_node->xdata = 0;
+        }
+    return new_node;
 }
 
 void node_delete(node_t *node)
 {
-    Py_XDECREF(node->key);
-    Py_XDECREF(node->value);
+    Py_XDECREF(KEY(node));
+    Py_XDECREF(VALUE(node));
     PyMem_Free(node);
 }
 
 void tree_clear(node_t * node)
 {
-    if LEFT_NODE(node) != NULL {
-        tree_clear(LEFT_NODE(node))
-    if RIGHT_NODE(node) != NULL {
-        tree_clear(RIGHT_NODE(node)
-    node_delete(node)
+    if (LEFT_NODE(node) != NULL)
+        {
+            tree_clear(LEFT_NODE(node));
+        };
+    if (RIGHT_NODE(node) != NULL)
+        {
+            tree_clear(RIGHT_NODE(node));
+        };
+    node_delete(node);
+}
+
+PyObject *node_get_value(node_t *node)
+{
+    Py_INCREF(NODE_KEY(node))
+    return NODE_KEY(node)
+}
+
+void node_set_value(node_t *node, PyObject *value)
+{
+    Py_INCREF(value)
+    NODE_VALUE(node) = value
+}
+
+PyObject *node_get_key(node_t *node)
+{
+    Py_INCREF(NODE_VALUE(node))
+    return NODE_VALUE(node)
+}
+
+void node_swap_data(node_t* node1, node_t* node2)
+{
+    PyObject *tmp
+    tmp = NODE_KEY(node1);
+    NODE_KEY(node1) = NODE_KEY(node2);
+    NODE_KEY(node2) = tmp
+    tmp = NODE_VALUE(node1);
+    NODE_VALUE(node1) = NODE_VALUE(node2);
+    NODE_VALUE(node2) = tmp
 }
--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("bintrees.cbintree", ["bintrees/cbintree.pyx"]),
+ext_modules = [Extension("bintrees.node", ["bintrees/node.c"]),
+               Extension("bintrees.cbintree", ["bintrees/cbintree.pyx"]),
                Extension("bintrees.crbtree", ["bintrees/crbtree.pyx"]),
                Extension("bintrees.cavltree", ["bintrees/cavltree.pyx"])]
 
