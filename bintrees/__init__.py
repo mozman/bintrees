c77f7cbc1e96220d86c18e08dd863f852ed68ac7
Manfred Moitzi <mozman@gmx.at>
2013-02-11 08:10:29 +0100

    minor edits

--- bintrees/__init__.py
+++ bintrees/__init__.py
@@ -8,7 +8,7 @@
 
 from __future__ import absolute_import
 
-"""
+__doc__ = """
 Binary Tree Package
 ===================
 
--- setup.py
+++ setup.py
@@ -17,38 +17,40 @@ try:
                    ]
     commands = {'build_ext': build_ext}
 except ImportError:
-    ext_modules =[]
+    ext_modules = []
     commands = {}
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'bintrees',
+    name='bintrees',
     version='1.0.1',
     description='Package provides Binary-, RedBlack- and AVL-Trees in Python and Cython.',
     author='mozman',
     url='http://bitbucket.org/mozman/bintrees',
     download_url='http://bitbucket.org/mozman/bintrees/downloads',
     author_email='mozman@gmx.at',
-    cmdclass = commands,
-    ext_modules = ext_modules,
+    cmdclass=commands,
+    ext_modules=ext_modules,
     packages=['bintrees'],
     long_description=read('README.txt'),
     platforms="OS Independent",
     license="LGPLv3",
     classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Cython",
-    "Programming Language :: Python :: Implementation :: CPython",
-    "Programming Language :: Python :: Implementation :: IronPython",
-    "Programming Language :: Python :: Implementation :: PyPy",
-    "Intended Audience :: Developers",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: IronPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
