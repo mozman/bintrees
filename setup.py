840dd0683f8b76f670c7f093cdccade34f01a32f
Manfred Moitzi <mozman@gmx.at>
2013-06-28 08:20:28 +0200

    fuck-branches

--- setup.py
+++ setup.py
@@ -13,10 +13,7 @@ from distutils.extension import Extension
 
 try:
     from Cython.Distutils import build_ext
-    ext_modules = [Extension("bintrees.cwalker", ["bintrees/ctrees.c", "bintrees/stack.c", "bintrees/cwalker.pyx"]),
-                   Extension("bintrees.qbintree", ["bintrees/ctrees.c", "bintrees/stack.c", "bintrees/qbintree.pyx"]),
-                   Extension("bintrees.qrbtree", ["bintrees/ctrees.c", "bintrees/stack.c", "bintrees/qrbtree.pyx"]),
-                   Extension("bintrees.qavltree", ["bintrees/ctrees.c", "bintrees/stack.c", "bintrees/qavltree.pyx"]),
+    ext_modules = [Extension("bintrees.cython_trees", ["bintrees/ctrees.c", "bintrees/stack.c", "bintrees/cython_trees.pyx"]),
                    ]
     commands = {'build_ext': build_ext}
 except ImportError:
@@ -29,7 +26,7 @@ def read(fname):
 
 setup(
     name='bintrees',
-    version='1.0.4',
+    version='2.0.0b1',
     description='Package provides Binary-, RedBlack- and AVL-Trees in Python and Cython.',
     author='mozman',
     url='http://bitbucket.org/mozman/bintrees',
@@ -38,11 +35,11 @@ setup(
     cmdclass=commands,
     ext_modules=ext_modules,
     packages=['bintrees'],
-    long_description=read('README.txt'),
+    long_description=read('README.txt')+read('NEWS.txt'),
     platforms="OS Independent",
     license="MIT License",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
@@ -50,7 +47,6 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Cython",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: IronPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
